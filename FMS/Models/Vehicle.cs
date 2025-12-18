44bd67257c0ee62eee30eb507ca0f2505a6e99cf
Hoàng Văn Tài <hoangvantai162@gmail.com>
2025-12-18 09:02:45 +0700

    early sketch for Driver,Maintenace,Vehicle,VehicleDriverAssignement model

---
+++ FMS/Models/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FMS.Models
+{
+    public class Vehicle
+    {
+        [Key] public int VehicleID { get; set; }
+        [Required, StringLength(100)] public string LicensePlate { get; set; }
+        [StringLength(100)] public string? VehicleModel { get; set; }
+
+        [StringLength(100)]public string? VehicleType { get; set; }
+        public int? ManufacturedYear { get; set; }
+        public int? CurrentKm { get; set; }
+        [StringLength(20)]  public string? VehicleStatus { get; set; }
+        [ForeignKey("Driver")] public int? DriverID { get; set; } //driver hien tai
+        public Driver? Driver { get; set; }
+
+        public ICollection<Maintenance>? Maintenances { get; set; }
+
+        public ICollection<VehicleDriverAssignment>? VehicleAssignments { get; set; }
+
+    }
+}
