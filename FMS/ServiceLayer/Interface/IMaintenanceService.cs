232618cecc0fdb5c59bdfcf6b91329f7df598e00
Hoàng Văn Tài <hoangvantai162@gmail.com>
2026-01-07 20:10:14 +0700

    Hoàn thành endpoint MaintenanceList, MaintenaceDetails, VehicleDetails

--- FMS/ServiceLayer/Interface/IMaintenanceService.cs
+++ FMS/ServiceLayer/Interface/IMaintenanceService.cs
@@ -1,9 +1,11 @@
 ﻿using FMS.Models;
+using FMS.ServiceLayer.DTO.MaintenanceDto;
 
 namespace FMS.ServiceLayer.Interface
 {
     public interface IMaintenanceService
     {
-        Task<List<Service>> GetAllServiceAsync();
+        Task<List<ServiceDto>> GetAllServiceAsync();
+        Task<List<MaintenanceListDto>> GetAllInvoiceAsync();
     }
 }
