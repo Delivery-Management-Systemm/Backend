557ccdd3848d3d29848f0dbceac057d904cf6d32
Hoàng Văn Tài <hoangvantai162@gmail.com>
2026-01-14 15:34:21 +0700

    UserService: Now use Phone instead of Email Booking: Create Booking

---
+++ FMS/ServiceLayer/DTO/UserDto/UserRegistrationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace FMS.ServiceLayer.DTO.UserDto
+{
+    // DTOs for request/response
+    public class UserRegistrationRequest
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
