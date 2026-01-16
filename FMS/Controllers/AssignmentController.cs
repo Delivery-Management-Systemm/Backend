c3687eb2aed058d33baa8672702bb7208847b58c
Hoàng Văn Tài <hoangvantai162@gmail.com>
2026-01-16 23:32:22 +0700

    CONTROLLER: TRIP ASSIGNMENT

---
+++ FMS/Controllers/AssignmentController.cs
@@ -0,0 +1,43 @@
+﻿using FMS.ServiceLayer.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FMS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AssignmentController: ControllerBase
+    {
+        private readonly ITripAssignmentService _tripAssignmentService;
+        public AssignmentController(ITripAssignmentService tripAssignmentService)
+        {
+            _tripAssignmentService = tripAssignmentService;
+        }
+
+        [HttpGet("{tripId}/available-vehicles")]
+        public async Task<IActionResult> GetAvailableVehiclesForTripAsync(int tripId)
+        {
+            var vehicles = await _tripAssignmentService.GetAvailableVehiclesForTripAsync(tripId);
+            return Ok(vehicles);
+        }
+
+        [HttpGet("vehicles/{vehicleId}/available-drivers")]
+        public async Task<IActionResult> GetAvailableDriversForVehicleAsync(int vehicleId)
+        {
+            var drivers = await _tripAssignmentService.GetAvailableDriversForVehicleAsync(vehicleId);
+            return Ok(drivers);
+        }
+
+        [HttpPost("{tripId}/assign")]
+        public async Task<IActionResult> AssignVehicleAndDriverAsync(
+            int tripId,int vehicleId, int driverId
+        )
+        {
+            await _tripAssignmentService.AssignVehicleAndDriverAsync(
+                tripId,
+                vehicleId,
+                driverId
+            );
+            return Ok(new { message = "Vehicle and driver assigned successfully." });
+        }
+    }
+}
