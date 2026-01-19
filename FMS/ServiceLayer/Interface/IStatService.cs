4dbaf05dc26d41090783f0b3ffd07d5c8f8c57b1
Hoàng Văn Tài <hoangvantai162@gmail.com>
2026-01-19 12:14:17 +0700

    stat controller, fix bug create vehicle

--- FMS/ServiceLayer/Interface/IStatService.cs
+++ FMS/ServiceLayer/Interface/IStatService.cs
@@ -4,6 +4,7 @@ namespace FMS.ServiceLayer.Interface
 {
     public interface IStatService
     {
+        Task<List<TopCardDto>> GetTopCardsAsync();
         Task<DashboardStatDto> GetDashboardStatsAsync();
     }
 }
